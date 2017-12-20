ee73cbcbee5dfc534c6876971d220c54fc16ca35
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2017-12-20 23:04:59 +0100

    source spring boot

--- src/main/java/fr/iagl/opl/wfuzz/MrRobot/MrRobotApplication.java
+++ src/main/java/fr/iagl/opl/wfuzz/MrRobot/MrRobotApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import fr.iagl.opl.wfuzz.MrRobot.entity.Files;
+import fr.iagl.opl.wfuzz.MrRobot.entity.File;
 import fr.iagl.opl.wfuzz.MrRobot.entity.Login;
 import fr.iagl.opl.wfuzz.MrRobot.repository.FilesRepository;
 import fr.iagl.opl.wfuzz.MrRobot.repository.LoginRepository;
@@ -30,8 +30,8 @@ public class MrRobotApplication {
 		loginRepository.save(new Login("arthur", "supersecure"));
 		loginRepository.save(new Login("marvin", "robot"));
 		loginRepository.save(new Login("ford", "h2g2"));
-		filesRepository.save(new Files("arthur", "arthur-description"));
-		filesRepository.save(new Files("arthur", "h2g2.jpeg"));
-		filesRepository.save(new Files("marvin", "marvin.jpeg"));
+		filesRepository.save(new File("arthur", "arthur-description"));
+		filesRepository.save(new File("arthur", "h2g2.jpeg"));
+		filesRepository.save(new File("marvin", "marvin.jpeg"));
 	}
 }
