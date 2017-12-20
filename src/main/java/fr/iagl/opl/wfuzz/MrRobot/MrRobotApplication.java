10be35d953b445ac9c7a4efef060fbac6a301132
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2017-12-20 10:25:02 +0100

    Springboot initial project

---
+++ src/main/java/fr/iagl/opl/wfuzz/MrRobot/MrRobotApplication.java
@@ -0,0 +1,12 @@
+package fr.iagl.opl.wfuzz.MrRobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrRobotApplication.class, args);
+	}
+}
