b92931844070ba3f24f7b355141a4fccd4189afc
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2017-12-21 15:25:13 +0100

    Update main.js

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -2,6 +2,8 @@ var loginForm = document.querySelector("#loginForm");
 var loginFormRaw = document.querySelector("#loginFormRaw");
 var errorLogin = document.querySelector("#errorLogin");
 
+var userName;
+
 function connect(event) {
 	event.preventDefault();
 	
@@ -15,9 +17,14 @@ function connect(event) {
 			if(status == "success"){
 				loginFormRaw.classList.add("hidden");
 				errorLogin.classList.add("hidden");
+				userName = $("#login").val();
+				listFiles();
 			}
 		}
 	).fail(function(xhr, status, error){errorLogin.classList.remove("hidden")});
+}
+
+function listFiles(){
 }
 
 loginForm.addEventListener("submit", connect, true);
